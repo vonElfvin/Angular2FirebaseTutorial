ac111112f0988ea08777e0688dc1be8493a3783f
Elvin Granat <elvgr805@student.liu.se>
2017-08-07 20:25:12 +0200

    Finished video 2: Added components, including navbar and pages for listings/add listings, as well as routes for these pages accessable through the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListingsComponent } from './components/listings/listings.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListingComponent } from './components/listing/listing.component';
+import { AddListingComponent } from './components/add-listing/add-listing.component';
+import { EditListingComponent } from './components/edit-listing/edit-listing.component';
+
+const myAppRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'annonser', component: ListingsComponent},
+  {path: 'skapa-annons', component: AddListingComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListingsComponent,
+    NavbarComponent,
+    ListingComponent,
+    AddListingComponent,
+    EditListingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(myAppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
